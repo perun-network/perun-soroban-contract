17c5259d490a83081f0b1ecba69a5becd054e0bb
Jan Bormet <jan.bormet@bormet.info>
2023-07-04 15:18:21 +0200

    test: Write simple test DSL

--- src/test.rs
+++ src/test.rs
@@ -14,66 +14,50 @@ use soroban_sdk::{Env, testutils::{Address as _, BytesN as _}, Address, BytesN};
 use token::Client as TokenClient;
 
 #[test]
-fn test() {
-    let env = Env::default();
-    env.mock_all_auths();
-    let client = AdjudicatorClient::new(&env, &env.register_contract(None, Adjudicator{}));
-    let nonce: BytesN<32> = BytesN::<32>::random(&env);
-    let key_alice = generate_keypair();
-    let key_bob = generate_keypair();
-    let alice = Participant { addr: Address::random(&env), pubkey: public_key(&env, &key_alice) };
-    let bob = Participant { addr: Address::random(&env), pubkey: public_key(&env, &key_bob) };
-    let params = Params {
-        a: alice.clone(),
-        b: bob.clone(),
-        nonce: nonce,
-        challenge_duration: 10,
-    };
-    let admin = Address::random(&env);
-    let token = TokenClient::new(&env, &env.register_stellar_asset_contract(admin.clone()));
+fn test_complete() {
+    let mut t = setup(10, 100, 200, true);
+    t.client.open(&t.params, &t.state);
+    t.verify_state(&t.state);
 
-    let state = State {
-        channel_id: get_channel_id(&env, &params),
-        balances: Balances {
-            token: token.address.clone(),
-            bal_a: 100,
-            bal_b: 200,
-        },
-        version: 0,
-        finalized: false,
-    };
-    let control = Control { funded_a: false, funded_b: false, closed: false,  withdrawn_a: false, withdrawn_b: false, disputed: false, timestamp: env.ledger().timestamp() };
-    let expected = make_channel(&params, &state, &control);
+    t.client.fund(&t.state.channel_id, &false);
+    t.verify_bal_contract(100);
+    t.verify_bal_a(0);
+    
+    t.client.fund(&t.state.channel_id, &true);
+    t.verify_bal_contract(300);
+    t.verify_bal_b(0);
 
-    client.open(&params, &state);
+    t.send_to_a(100);
 
+    t.finalize();
 
-    assert_eq!(
-        client.get_channel(&state.channel_id), expected);
+    t.client.close(&t.state, &t.sig_a(), &t.sig_b());
+    t.verify_state(&t.state);
+    t.verify_bal_contract(300);
 
-    token.mint(&alice.addr, &100i128);
-    token.mint(&bob.addr, &200i128);
+    t.client.withdraw(&t.state.channel_id, &false);
+    t.verify_bal_a(200);
+    t.verify_bal_contract(100);
+    
+    t.client.withdraw(&t.state.channel_id, &true);
+    t.verify_bal_b(100);
+    t.verify_bal_contract(0);
+}
 
-    client.fund(&state.channel_id, &false);
+#[test]
+fn test_funding_abort() {
+    let t = setup(10, 100, 200, true);
 
-    client.fund(&state.channel_id, &true);
+    t.client.open(&t.params, &t.state);
+    t.verify_state(&t.state);
+    
+    t.client.fund(&t.channel_id, &false);
+    t.verify_bal_contract(100);
+    t.verify_bal_a(0);
 
-    let mut final_state = state.clone();
-    final_state.finalized = true;
-    final_state.version = 1;
-    final_state.balances.bal_a = 200;
-    final_state.balances.bal_b = 100;
-    let sig_a = sign(&env, &key_alice, &final_state);
-    let sig_b = sign(&env, &key_bob, &final_state);
-    assert_eq!(token.balance(&alice.addr),0);
-    assert_eq!(token.balance(&bob.addr),0);
-
-    client.close(&final_state, &sig_a, &sig_b);
-    assert_eq!(client.get_channel(&final_state.channel_id).state, final_state);
-    client.withdraw(&final_state.channel_id, &false);
-    assert_eq!(token.balance(&alice.addr),200i128);
-    client.withdraw(&final_state.channel_id, &true);
-    assert_eq!(token.balance(&bob.addr),100i128);
+    t.client.abort_funding(&t.channel_id);
+    t.verify_bal_contract(0);
+    t.verify_bal_a(100);
 }
 
 
@@ -92,4 +76,129 @@ fn public_key(e: &Env, signer: &Keypair) -> BytesN<32> {
 
 fn generate_keypair() -> Keypair {
     Keypair::generate(&mut thread_rng())
+}
+
+fn setup(challenge_duration: u64, bal_a: i128, bal_b: i128, mock_auth: bool) ->  Test<'static> {
+    let e = Env::default();
+    if mock_auth {
+        e.mock_all_auths();
+    }
+    let key_alice = generate_keypair();
+    let key_bob = generate_keypair();
+    let alice = Participant { addr: Address::random(&e), pubkey: public_key(&e, &key_alice) };
+    let bob = Participant { addr: Address::random(&e), pubkey: public_key(&e, &key_bob) };
+    let token = TokenClient::new(&e, &e.register_stellar_asset_contract(Address::random(&e)));
+    token.mint(&alice.addr, &bal_a);
+    token.mint(&bob.addr, &bal_b);
+    let params = Params {
+        a: alice.clone(),
+        b: bob.clone(),
+        nonce: BytesN::<32>::random(&e),
+        challenge_duration: challenge_duration,
+    };
+    let channel_id = get_channel_id(&e, &params);
+    let state = State {
+        channel_id: channel_id.clone(),
+        balances: Balances {
+            token: token.address.clone(),
+            bal_a: bal_a,
+            bal_b: bal_b,
+        },
+        version: 0,
+        finalized: false,
+    };
+    let client = AdjudicatorClient::new(&e, &e.register_contract(None, Adjudicator{}));
+    Test { env: e, alice, bob, key_alice, key_bob, params, channel_id, state, client, token }
+}
+
+fn verify_state(t: &Test, state: &State) {
+    assert_eq!(&t.client.get_channel(&state.channel_id).state, state);
+}
+
+fn sign_state(t: &Test, state: &State) -> (BytesN<64>, BytesN<64>) {
+    let sig_a = sign(&t.env, &t.key_alice, &state);
+    let sig_b = sign(&t.env, &t.key_bob, &state);
+    (sig_a, sig_b)
+}
+
+struct Test<'a> {
+    env: Env,
+    alice: Participant,
+    bob: Participant,
+    key_alice: Keypair,
+    key_bob: Keypair,
+    params: Params,
+    channel_id: BytesN<32>,
+    state: State,
+    client: AdjudicatorClient<'a>,
+    token: TokenClient<'a>,
+} 
+
+impl Test<'_> {
+    fn verify_state(&self, state: &State) {
+        assert_eq!(&self.client.get_channel(&state.channel_id).state, state);
+    }
+
+    fn update(&mut self, new_state: State) {
+        self.state = new_state.clone();
+    }
+
+    fn sign_state(&self, state: &State) -> (BytesN<64>, BytesN<64>) {
+        let sig_a = sign(&self.env, &self.key_alice, &state);
+        let sig_b = sign(&self.env, &self.key_bob, &state);
+        (sig_a, sig_b)
+    }
+
+    fn send_to_a(&mut self, amt: i128) {
+        self.update(State { 
+            channel_id: self.state.channel_id.clone(), 
+            balances: Balances { 
+                token: self.state.balances.token.clone(), 
+                bal_a: self.state.balances.bal_a + amt, 
+                bal_b: self.state.balances.bal_b - amt }, 
+            version: self.state.version +1, 
+            finalized: self.state.finalized })
+    }
+
+    fn send_to_b(&mut self, amt: i128) {
+        self.update(State { 
+            channel_id: self.state.channel_id.clone(), 
+            balances: Balances { 
+                token: self.state.balances.token.clone(), 
+                bal_a: self.state.balances.bal_a - amt, 
+                bal_b: self.state.balances.bal_b + amt }, 
+            version: self.state.version +1, 
+            finalized: self.state.finalized })
+    }
+
+    fn finalize(&mut self) {
+        self.update(State { 
+            version: self.state.version +1, 
+            finalized: true,
+            ..self.state.clone() })
+    }
+
+    fn sig_a(&self) -> BytesN<64> {
+        sign(&self.env, &self.key_alice, &self.state)
+    }
+
+    fn sig_b(&self) -> BytesN<64> {
+        sign(&self.env, &self.key_bob, &self.state)
+    }
+
+    fn sigs(&self) -> (BytesN<64>, BytesN<64>) {
+        (self.sig_a(), self.sig_b())
+    }
+
+    fn verify_bal_a(&self, bal: i128) {
+        assert_eq!(self.token.balance(&self.alice.addr), bal);
+    }
+
+    fn verify_bal_b(&self, bal: i128) {
+        assert_eq!(self.token.balance(&self.bob.addr), bal);
+    }
+
+    fn verify_bal_contract(&self, bal: i128) {
+        assert_eq!(self.token.balance(&self.client.address), bal);
+    }
 }
